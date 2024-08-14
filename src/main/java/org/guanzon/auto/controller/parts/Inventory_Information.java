81cebd377267663c4394887a10b2bcd1a2978586
xurpas7 <83201804+mtcuison07@users.noreply.github.com>
2024-08-14 15:20:05 +0800

    Initial commit

---
+++ src/main/java/org/guanzon/auto/controller/parts/Inventory_Information.java
@@ -0,0 +1,198 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.guanzon.auto.controller.parts;
+
+import org.guanzon.appdriver.agent.ShowDialogFX;
+import org.guanzon.appdriver.base.GRider;
+import org.guanzon.appdriver.base.MiscUtil;
+import org.guanzon.appdriver.base.SQLUtil;
+import org.guanzon.appdriver.constant.EditMode;
+import org.guanzon.appdriver.iface.GRecord;
+import org.guanzon.auto.parts.model.Model_Inventory_Master;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author Arsiela
+ */
+public class Inventory_Information implements GRecord {
+
+    GRider poGRider;
+    boolean pbWthParent;
+    int pnEditMode;
+    String psRecdStat;
+
+    Model_Inventory_Master poModel;
+    JSONObject poJSON;
+
+    public Inventory_Information(GRider foGRider, boolean fbWthParent) {
+        poGRider = foGRider;
+        pbWthParent = fbWthParent;
+
+        poModel = new Model_Inventory_Master(foGRider);
+        pnEditMode = EditMode.UNKNOWN;
+    }
+
+    @Override
+    public int getEditMode() {
+        return pnEditMode;
+    }
+
+    @Override
+    public void setRecordStatus(String fsValue) {
+        psRecdStat = fsValue;
+    }
+
+    @Override
+    public JSONObject setMaster(int fnCol, Object foData) {
+        return poModel.setValue(fnCol, foData);
+    }
+
+    @Override
+    public JSONObject setMaster(String fsCol, Object foData) {
+        return poModel.setValue(fsCol, foData);
+    }
+
+    @Override
+    public Object getMaster(int fnCol) {
+        return poModel.getValue(fnCol);
+    }
+
+    @Override
+    public Object getMaster(String fsCol) {
+        return poModel.getValue(fsCol);
+    }
+
+    @Override
+    public JSONObject newRecord() {
+        return poModel.newRecord();
+    }
+
+    @Override
+    public JSONObject openRecord(String fsValue) {
+        return poModel.openRecord(fsValue);
+    }
+
+    @Override
+    public JSONObject updateRecord() {
+        JSONObject loJSON = new JSONObject();
+
+        if (poModel.getEditMode() == EditMode.UPDATE) {
+            loJSON.put("result", "success");
+            loJSON.put("message", "Edit mode has changed to update.");
+        } else {
+            loJSON.put("result", "error");
+            loJSON.put("message", "No record loaded to update.");
+        }
+        return loJSON;
+    }
+
+    @Override
+    public JSONObject saveRecord() {
+        if (!pbWthParent) {
+            poGRider.beginTrans();
+        }
+
+        poJSON = poModel.saveRecord();
+
+        if ("success".equals((String) poJSON.get("result"))) {
+            if (!pbWthParent) {
+                poGRider.commitTrans();
+            }
+        } else {
+            if (!pbWthParent) {
+                poGRider.rollbackTrans();
+            }
+        }
+        return poJSON;
+    }
+
+    @Override
+    public JSONObject deleteRecord(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public JSONObject deactivateRecord(String fsValue) {
+        poJSON = new JSONObject();
+
+        if (poModel.getEditMode() == EditMode.UPDATE) {
+            poJSON = poModel.setActive(false);
+
+            if ("error".equals((String) poJSON.get("result"))) {
+                return poJSON;
+            }
+
+            poJSON = poModel.saveRecord();
+        } else {
+            poJSON = new JSONObject();
+            poJSON.put("result", "error");
+            poJSON.put("message", "No record loaded to update.");
+        }
+        return poJSON;
+    }
+
+    @Override
+    public JSONObject activateRecord(String fsValue) {
+        poJSON = new JSONObject();
+
+        if (poModel.getEditMode() == EditMode.UPDATE) {
+            poJSON = poModel.setActive(true);
+
+            if ("error".equals((String) poJSON.get("result"))) {
+                return poJSON;
+            }
+
+            poJSON = poModel.saveRecord();
+        } else {
+            poJSON = new JSONObject();
+            poJSON.put("result", "error");
+            poJSON.put("message", "No record loaded to update.");
+        }
+        return poJSON;
+    }
+
+    @Override
+    public JSONObject searchRecord(String fsValue, boolean fbByCode) {
+        String lsCondition = "";
+
+        if (psRecdStat.length() > 1) {
+            for (int lnCtr = 0; lnCtr <= psRecdStat.length() - 1; lnCtr++) {
+                lsCondition += ", " + SQLUtil.toSQL(Character.toString(psRecdStat.charAt(lnCtr)));
+            }
+
+            lsCondition = "cRecdStat IN (" + lsCondition.substring(2) + ")";
+        } else {
+            lsCondition = "cRecdStat = " + SQLUtil.toSQL(psRecdStat);
+        }
+
+        String lsSQL = MiscUtil.addCondition(poModel.makeSelectSQL(), " sDescript LIKE "
+                + SQLUtil.toSQL(fsValue + "%") + " AND " + lsCondition);
+
+        poJSON = ShowDialogFX.Search(poGRider,
+                lsSQL,
+                fsValue,
+                "Bar Code»Description",
+                "sBarCodex»sDescript",
+                "sBarCodex»sDescript",
+                fbByCode ? 0 : 1);
+
+        if (poJSON != null) {
+            return poModel.openRecord((String) poJSON.get("sStockIDx"));
+        } else {
+            poJSON.put("result", "error");
+            poJSON.put("message", "No record loaded to update.");
+            return poJSON;
+        }
+    }
+
+    @Override
+    public Model_Inventory_Master getModel() {
+        return poModel;
+    }
+    
+    
+}
